802112b98f1ffd7ebd57c4dc1848c2ec5e32ef46
kimjinwoo <rlawlsdn177@naver.com>
2023-09-18 16:38:04 +0900

    fix : className 충돌 수정

--- src/common/organisms/NavMobile.tsx
+++ src/common/organisms/NavMobile.tsx
@@ -3,34 +3,34 @@ import './NavMobile.scss';
 
 const NavMobile = () => {
   return (
-    <div className="navbar">
-      <div className="navbar__item">
-        <img src="home.png" alt="메인 페이지" className="navbar__item__logo"/>
-        <p className="navbar__item__text">메인</p>
-        <img src="select.png" className="navbar__item__bar"/>
+    <div className="navbar_m">
+      <div className="navbar_m__item">
+        <img src="home.png" alt="메인 페이지" className="navbar_m__item__logo"/>
+        <p className="navbar__item__text_m">메인</p>
+        <img src="select.png" className="navbar_m__item__bar"/>
       </div>
       <div>
-        <img src="select.png" className="navbar__bar"/>
+        <img src="select.png" className="navbar_m__bar"/>
       </div>
-      <div className="navbar__item">
-        <img src="honor.png" alt="명예의 전당" className="navbar__item__logo"/>
-        <p className="navbar__item__text">명예의 전당</p>
-        <img src="select.png" className="navbar__item__bar"/>
-      </div>
-      <div>
-        <img src="select.png" className="navbar__bar"/>
-      </div>
-      <div className="navbar__item">
-        <img src="weekly.png" alt="주간 네컷" className="navbar__item__logo"/>
-        <p className="navbar__item__text">주간 네컷</p>
-        <img src="select.png" className="navbar__item__bar"/>
+      <div className="navbar_m__item">
+        <img src="honor.png" alt="명예의 전당" className="navbar_m__item__logo"/>
+        <p className="navbar_m__item__text">명예의 전당</p>
+        <img src="select.png" className="navbar_m__item__bar"/>
       </div>
       <div>
-        <img src="select.png" className="navbar__bar"/>
+        <img src="select.png" className="navbar_m__bar"/>
       </div>
-      <div className="navbar__item">
-        <img src="user_logo.png" alt="마이 페이지" className="navbar__item__logo"/>
-        <p className="navbar__item__text">마이 페이지</p>
+      <div className="navbar_m__item">
+        <img src="weekly.png" alt="주간 네컷" className="navbar_m__item__logo"/>
+        <p className="navbar_m__item__text">주간 네컷</p>
+        <img src="select.png" className="navbar_m__item__bar"/>
+      </div>
+      <div>
+        <img src="select.png" className="navbar_m__bar"/>
+      </div>
+      <div className="navbar_m__item">
+        <img src="user_logo.png" alt="마이 페이지" className="navbar_m__item__logo"/>
+        <p className="navbar_m__item__text">마이 페이지</p>
       </div>
     </div>
   );
