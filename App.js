afe1cf9057ab62de84c6796dec5392949b952484
Jinwoo Kim <80628077+KIMJINWOO4@users.noreply.github.com>
2023-09-19 19:50:05 +0900

    feat: 디중 type을 canvas

---
+++ App.js
@@ -0,0 +1,13 @@
+// src/App.js
+import React from "react";
+import Canvas from "./canvas";
+
+function App() {
+  return (
+    <div className="App">
+      <Canvas />
+    </div>
+  );
+}
+
+export default App;
