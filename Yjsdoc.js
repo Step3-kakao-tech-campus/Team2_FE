9bbc1e3a940cddc1baee0375c5bb3414127269eb
kimjinwoo <rlawlsdn177@naver.com>
2023-09-19 19:54:32 +0900

    fix: 경로 오류 파일 삭제

--- Yjsdoc.js
+++
@@ -1,8 +0,0 @@
-// src/YjsDoc.js
-import * as Y from "yjs";
-import { WebsocketProvider } from "y-websocket";
-
-const ydoc = new Y.Doc();
-const provider = new WebsocketProvider("ws://localhost:1234", "my-room", ydoc);
-
-export const yArray = ydoc.getArray("stars");
