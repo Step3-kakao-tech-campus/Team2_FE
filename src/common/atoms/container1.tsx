bd36566d428457249797b82893f575d4e28bb834
charmeee <mandugohigh@gmail.com>
2023-09-20 14:03:05 +0900

    fix: conflict 되는 부분수정.
    - router 합침
    - 컨테이너 일단은 파일 분리해놓음..

---
+++ src/common/atoms/container1.tsx
@@ -0,0 +1,36 @@
+import './container.scss';
+import React from 'react';
+
+interface DescriptionContainerProps {
+    width?: string;
+    height?: string;
+    title: string;
+    description: string;
+    isCenter?: boolean;
+}
+
+export const IntroContainer = ({ children }: { children: React.ReactNode }) => {
+    return <div className="container main">{children}</div>;
+};
+
+export const Container = ({ children }: { children: React.ReactNode }) => {
+    return <div className="container">{children}</div>;
+};
+
+export const DescriptionContainer = ({
+    width,
+    height,
+    title,
+    description,
+    isCenter = false,
+}: DescriptionContainerProps) => {
+    return (
+        <div
+            className={`description ${isCenter && 'center'}`}
+            style={{ width, height }}
+        >
+            <h2>{title}</h2>
+            <p>{description}</p>
+        </div>
+    );
+};
