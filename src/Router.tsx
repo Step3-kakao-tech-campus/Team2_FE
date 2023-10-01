629c70cd0ec817a9a7c4dae079b4d4bb2c1b1da6
kimjinwoo <rlawlsdn177@naver.com>
2023-10-01 17:56:53 +0900

    feat: albumcreate Route 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,13 +2,15 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/Login';
 import Layout from './common/templates/Layout';
 import LandingPage from './pages/Landing';
-import Canvas from './pages/canvas';
+import Canvas from './pages/Canvas';
+import AlbumCreationPage from './pages/Album/AlbumCreate';
 
 const Router = () => (
     <BrowserRouter>
         <Routes>
             <Route element={<Layout />}>
                 <Route path="/" element={<LandingPage />} />
+                <Route path="albumcreate" element={<AlbumCreationPage/>}/>
             </Route>
             <Route path="login" element={<LoginPage />} />
             <Route path="canvas" element={<Canvas />} />
