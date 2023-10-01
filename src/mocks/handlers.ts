74aa289a4497cc2721b0ce185d801a9890570831
charmeee <mandugohigh@gmail.com>
2023-10-01 14:53:48 +0900

    feat: 데이터 추가

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -15,13 +15,25 @@ const albumList = {
         albums: [
             {
                 id: 1,
-                image: '링크',
+                image: 'main_ carousel1.png',
                 albumName: '또 너야 지석진',
                 description: '수용이와 석진이의 프라이빗한 공간',
             },
             {
                 id: 2,
-                image: '링크',
+                image: 'main_ carousel1.png',
+                albumName: '우리 우정 스테이^^',
+                description: '이뿌니 6인방 우정 뽀에버',
+            },
+            {
+                id: 3,
+                image: 'main_ carousel1.png',
+                albumName: '또 너야 지석진',
+                description: '수용이와 석진이의 프라이빗한 공간',
+            },
+            {
+                id: 4,
+                image: 'main_ carousel1.png',
                 albumName: '우리 우정 스테이^^',
                 description: '이뿌니 6인방 우정 뽀에버',
             },
