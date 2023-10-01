560f99cfd0bf8bc8398dd5fef65a3cd5a9a67219
kimjinwoo <rlawlsdn177@naver.com>
2023-10-01 11:35:43 +0900

    feat: 앨범 생성 페이지 Route 추가

--- src/Router.tsx
+++ src/Router.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LoginPage from './pages/Login';
 import Layout from './common/templates/Layout';
 import LandingPage from './pages/Landing';
+import AlbumCreationPage from './pages/Album/AlbumCreate';
 import Canvas from './pages/canvas';
 
 const Router = () => (
@@ -9,6 +10,7 @@ const Router = () => (
         <Routes>
             <Route element={<Layout />}>
                 <Route path="/" element={<LandingPage />} />
+                <Route path="albumcreate" element={<AlbumCreationPage/>}/>
             </Route>
             <Route path="login" element={<LoginPage />} />
             <Route path="canvas" element={<Canvas />} />
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -6,11 +6,54 @@ import { LocalImage } from '../../common/atoms/image';
 import { Form, FormItem } from '../../common/atoms/Form';
 import Button from '../../common/atoms/Button';
 
-const LoginPage: React.FC = () => {
-    const handleLoginClick = () => {
-        console.log(`Logging in`);
-    };
+declare const window: Window & { Kakao: any, google: any };
 
+const LoginPage: React.FC = () => {
+    React.useEffect(() => {
+        if (window.Kakao) {
+          const kakao = window.Kakao;
+          if (!kakao.isInitialized()) {
+            kakao.init(process.env.REACT_APP_KAKAO_JS_KEY);
+          }
+        }
+      }, []);
+      const buttonRef = React.useRef<HTMLDivElement>(null); // Specify the type of the ref
+
+    React.useEffect(() => {
+    if (buttonRef.current) { // Make sure the ref is not null
+      window.google.accounts.id.initialize({
+        client_id: process.env.REACT_APP_GOOGLE_CLIENT_KEY,
+        callback: handleCredentialResponse
+      });
+
+      window.google.accounts.id.renderButton(
+        buttonRef.current,
+        { theme: 'outline', size: 'large' },  // Customize the button appearance here.
+      );
+
+      // Add an event listener to the Google login button.
+      buttonRef.current.addEventListener('click', handleClickGoogle);
+    }
+  }, []);
+
+  function handleCredentialResponse(response: any) { // Specify a type for the response
+    console.log(response);
+  }
+
+  function handleClickGoogle() {
+     window.google.accounts.id.prompt(); // Will prompt the user to select their account when the button is clicked.
+   }
+
+      const loginWithKakao = () => {
+        window.Kakao.Auth.login({
+          success: function(authObj: any) {
+            console.log(JSON.stringify(authObj)); //인증 완료 토큰
+          },
+          fail: function(err: any) {
+            alert(JSON.stringify(err));
+          },
+        });
+      };
     return (
         <MainContainer className="column">
             <div className="logo">
@@ -21,7 +64,7 @@ const LoginPage: React.FC = () => {
                     <FormItem>
                         <Button
                             className="login kakao"
-                            onClick={handleLoginClick}
+                            onClick={loginWithKakao}
                             imageSrc="logo_kakao.png"
                         >
                             카카오톡 계정으로 로그인
@@ -31,7 +74,7 @@ const LoginPage: React.FC = () => {
                     <FormItem>
                         <Button
                             className="login google"
-                            onClick={handleLoginClick}
+                            onClick={handleClickGoogle}
                             imageSrc="logo_google.png"
                         >
                             구글 계정으로 로그인
