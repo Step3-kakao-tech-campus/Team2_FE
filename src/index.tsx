a4f7546ff0efa587bd578c9037ba001236244dc3
charmeee <mandugohigh@gmail.com>
2023-10-01 14:51:42 +0900

    refactor: 조금더 일반적으로 쓰이는 파일이름으로 변경

--- src/index.tsx
+++ src/index.tsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { worker } from './mocks/worker';
+import { worker } from './mocks/browser';
 
 if (process.env.NODE_ENV === 'development') {
     worker.start();
