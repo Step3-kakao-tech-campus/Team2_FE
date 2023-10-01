f8e641cb8368f09ff25bc8e5bed7b24905f3a311
charmeee <mandugohigh@gmail.com>
2023-10-01 14:56:58 +0900

    feat: 앨범 그룹 페이지 추가

--- src/pages/AlbumGroup/index.tsx
+++ src/pages/AlbumGroup/index.tsx
@@ -3,42 +3,35 @@ import React from 'react';
 import { MainContainer } from '../../common/atoms/Container';
 import { AlbumCard } from './componenents/AlbumCard';
 import WrapContainer from './componenents/WrapContainer';
+import { useQuery } from 'react-query';
+import albumApi from '../../service/album';
+import StatusLayOut from '../../common/templates/StatusLayOut';
 const AlbumGroupPage = () => {
+    const userId = '1';
+    const { isLoading, isError, data, error } = useQuery({
+        queryKey: ['albumGroup', userId],
+        queryFn: albumApi.getAlbumGroup,
+    });
+
+    console.log('data', data);
+    //이렇게해야 타입도움을 받을수잇는듯?
+
     return (
         <MainContainer>
             <LocalImage src="main_ carousel1.png" height="300px" />
-            <WrapContainer>
-                <AlbumCard
-                    image="main_ carousel1.png"
-                    groupId="1"
-                    groupName="그룹이름ddd"
-                    groupDescription="dffafdadfdsafdaf"
-                />
-                <AlbumCard
-                    image="main_ carousel1.png"
-                    groupId="1"
-                    groupName="그룹이름ddd"
-                    groupDescription="dffafdadfdsafdaf"
-                />
-                <AlbumCard
-                    image="main_ carousel1.png"
-                    groupId="1"
-                    groupName="그룹이름ddd"
-                    groupDescription="dffafdadfdsafdaf"
-                />
-                <AlbumCard
-                    image="main_ carousel1.png"
-                    groupId="1"
-                    groupName="그룹이름ddd"
-                    groupDescription="dffafdadfdsafdaf"
-                />
-                <AlbumCard
-                    image="main_ carousel1.png"
-                    groupId="1"
-                    groupName="그룹이름ddd"
-                    groupDescription="dffafdadfdsafdaf"
-                />
-            </WrapContainer>
+            <StatusLayOut isLoading={isLoading} isError={isError} error={error}>
+                <WrapContainer>
+                    {data?.albums.map(albumGroup => (
+                        <AlbumCard
+                            key={albumGroup.id}
+                            image={albumGroup.image}
+                            groupId={albumGroup.id}
+                            groupName={albumGroup.albumName}
+                            groupDescription={albumGroup.description}
+                        />
+                    ))}
+                </WrapContainer>
+            </StatusLayOut>
         </MainContainer>
     );
 };
