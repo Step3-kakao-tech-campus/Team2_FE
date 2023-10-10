72670ae2c728907c40add63df5c7f1653eea26fd
kimjinwoo <rlawlsdn177@naver.com>
2023-10-10 14:59:50 +0900

    fix: Route 경로 계층화

--- src/Router.tsx
+++ src/Router.tsx
@@ -13,8 +13,11 @@ const Router = () => (
             <Route element={<Layout />}>
                 <Route path="/" element={<LandingPage />} />
                 <Route path="account" element={<AccountPage />} />
-                <Route path="albumcreate" element={<AlbumCreationPage/>}/>
-                <Route path="album" element={<AlbumGroupPage />} />
+
+                <Route path="album">
+                    <Route index element={<AlbumGroupPage />} />
+                    <Route path="create" element={<AlbumCreationPage />} />
+                </Route>
             </Route>
             <Route path="login" element={<LoginPage />} />
             <Route path="canvas" element={<Canvas />} />
