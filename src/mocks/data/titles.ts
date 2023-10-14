afbd25cb701ad9121c99f3291372c34007bd35e4
kimjinwoo <rlawlsdn177@naver.com>
2023-10-14 14:59:50 +0900

    test: mock data 추가

---
+++ src/mocks/data/titles.ts
@@ -0,0 +1,29 @@
+export const titles = {
+    success: true,
+    response: {
+        titles: [
+            {
+				titleId:1,
+				titleName: "네컷 인플루언서",
+                titleGrade: "LEVEL_GOLD",
+                titleSub: "우리가 부럽다고 ?!",
+                titleDescription: "우리 그룹 사진의 좋아요가 1M개 이상이에요.",
+			},
+            {
+				titleId:2,
+				titleName: "선체 파괴자",
+                titleGrade: "LEVEL_GOLD",
+                titleSub: "우리가 부럽다고 ?!",
+                titleDescription: "우리 그룹 사진의 좋아요가 1M개 이상이에요.",
+			},
+            {
+				titleId:3,
+				titleName: "불멸의 철갑궁",
+                titleGrade: "LEVEL_GOLD",
+                titleSub: "우리가 부럽다고 ?!",
+                titleDescription: "우리 그룹 사진의 좋아요가 1M개 이상이에요.",
+			},
+        ],
+    },
+    error: null,
+};
