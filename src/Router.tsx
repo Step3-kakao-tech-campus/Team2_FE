4f062cb70f31e1951736d0504524c302a3eeec62
kimjinwoo <rlawlsdn177@naver.com>
2023-11-06 20:05:18 +0900

    feat: Invite Page 구현

--- src/Router.tsx
+++ src/Router.tsx
@@ -12,6 +12,7 @@ import AlbumViewPage from './pages/Album/View';
 import RedirectPage from './pages/Login/redirect';
 import ErrorPage from './pages/Common/Error';
 import Flip from './pages/Flip';
+import AlbumInvitePage from './pages/Album/Invite';
 
 const Router = () => (
     <BrowserRouter>
@@ -24,6 +25,7 @@ const Router = () => (
                     <Route index element={<AlbumGroupPage />} />
                     <Route path="create" element={<AlbumCreationPage />} />
                     <Route path="view" element={<AlbumViewPage />} />
+                    <Route path="invite" element={<AlbumInvitePage />} />
                 </Route>
             </Route>
             <Route path="login">
