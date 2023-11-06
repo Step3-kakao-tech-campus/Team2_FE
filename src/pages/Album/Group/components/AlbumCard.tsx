c5588ccbf92ca59258ca8c02816291e199a7e1a9
kimjinwoo <rlawlsdn177@naver.com>
2023-11-06 17:41:02 +0900

    feat: 공유 버튼 추가

--- src/pages/Album/Group/components/AlbumCard.tsx
+++ src/pages/Album/Group/components/AlbumCard.tsx
@@ -31,8 +31,20 @@ export const AlbumCard = ({
 };
 
 const ButtonGroup = ({ id }: { id: String }) => {
+    const invitationLink = `https://example.com/invite?albumId=${id}`;
+
+    const copyToClipboard = async () => {
+        try {
+            await navigator.clipboard.writeText(invitationLink);
+            alert('링크가 클립보드에 복사되었습니다.'); // 사용자에게 피드백 제공
+        } catch (err) {
+            console.error('클립보드에 복사하지 못했습니다.', err);
+            alert('클립보드에 복사하지 못했습니다.');
+        }
+    };
     return (
         <div className="album_btn_group">
+            <button onClick={copyToClipboard}>공유</button>
             <button>수정</button>
             <button>삭제</button>
         </div>
