d36cd20e3bf3e89a8f21dc999010de907f033315
charmeee <mandugohigh@gmail.com>
2023-11-06 15:23:35 +0900

    feat: 유저 정보 요청 api생성

--- src/service/user.ts
+++ src/service/user.ts
@@ -1,5 +1,5 @@
 import httpClient from './index';
-
+import { User } from '../recoil/user';
 type Vendor = 'kakao' | 'google';
 
 interface OauthLoginRequest {
@@ -10,4 +10,5 @@ interface OauthLoginRequest {
 export const userApi = {
     oauthLogin: ({ vendor, authCode }: OauthLoginRequest): Promise<String> =>
         httpClient.post(`/auth/login/${vendor}`, authCode),
+    getUser: (): Promise<User> => httpClient.get('/user'),
 };
