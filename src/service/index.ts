cf404a8247b1d93d3ca6e4a0be66bc27fc4dcb38
charmeee <mandugohigh@gmail.com>
2023-11-11 00:17:14 +0900

    fix: baseurl 수정

--- src/service/index.ts
+++ src/service/index.ts
@@ -1,14 +1,18 @@
 import axios from 'axios';
-
 export interface CustomError {
     status: number;
     message: string;
 }
 
+const baseURL = () => {
+    if (process.env.NODE_ENV === 'production') {
+        return (process.env.REACT_APP_API_URL || '') + '/api';
+    }
+    return '/api';
+};
+
 const httpClient = axios.create({
-    baseURL:
-        (process.env.NODE_ENV === 'production' &&
-            (process.env.REACT_APP_API_URL || '')) + '/api',
+    baseURL: baseURL(),
     timeout: 10000,
     headers: {
         'Content-Type': 'application/json',
