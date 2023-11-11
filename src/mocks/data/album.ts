3951bcfce4694cf5ae53c700923f271ed29889b8
charmeee <mandugohigh@gmail.com>
2023-11-11 22:10:16 +0900

    feat: 목업 데이터 변경

--- src/mocks/data/album.ts
+++ src/mocks/data/album.ts
@@ -4,25 +4,25 @@ export const albumList = {
         albums: [
             {
                 id: 1,
-                image: 'main_ carousel1.png',
+                image: 'a.jpeg',
                 albumName: '또 너야 지석진',
                 description: '수용이와 석진이의 프라이빗한 공간',
             },
             {
                 id: 2,
-                image: 'main_ carousel1.png',
+                image: 'b.jpeg',
                 albumName: '우리 우정 스테이^^',
                 description: '이뿌니 6인방 우정 뽀에버',
             },
             {
                 id: 3,
-                image: 'main_ carousel1.png',
+                image: 'c.jpeg',
                 albumName: '또 너야 지석진',
                 description: '수용이와 석진이의 프라이빗한 공간',
             },
             {
                 id: 4,
-                image: 'main_ carousel1.png',
+                image: 'd.jpeg',
                 albumName: '우리 우정 스테이^^',
                 description: '이뿌니 6인방 우정 뽀에버',
             },
